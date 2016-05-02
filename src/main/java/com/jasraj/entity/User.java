7784a8d6c2d8a9f8bcc2002fceed3c4dfe683556
krnsingh <karanjeet@live.co.uk>
2016-05-02 17:06:27 +0100

    corrected merge mistake of user class

--- src/main/java/com/jasraj/entity/User.java
+++ src/main/java/com/jasraj/entity/User.java
@@ -1,21 +1,34 @@
 package com.jasraj.entity;
 
-/**
- * Created by Jaskirat on 29-04-2016.
- */
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Created by Jaskirat on 29-04-2016.
- */
 @Entity
-@Table(name="username",
-        uniqueConstraints={@UniqueConstraint(columnNames = {"id"})})
+@Table(uniqueConstraints =
+@UniqueConstraint(columnNames = {"emailId"}))
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String name;
+    private String emailId;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Alert> alerts;
+
+    public List<Alert> getAlerts() {
+        return alerts;
+    }
+
+    public void setAlerts(List<Alert> alerts) {
+        this.alerts = alerts;
+    }
+
+    public void addAlert(Alert alert) {
+        if (alerts == null || alerts.size() == 0) {
+            alerts = new ArrayList<>();
+        }
+        alerts.add(alert);
+    }
 
     public int getId() {
         return id;
@@ -25,12 +38,11 @@ public class User {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getEmailId() {
+        return emailId;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
     }
-
 }
